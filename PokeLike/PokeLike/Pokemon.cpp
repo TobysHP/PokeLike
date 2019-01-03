5933934092db4078921564db06f2986e39381802
TobysHP <36991491+TobysHP@users.noreply.github.com>
2019-01-03 13:26:45 +0100

    Update Pokemon.cpp

--- PokeLike/PokeLike/Pokemon.cpp
+++ PokeLike/PokeLike/Pokemon.cpp
@@ -5,9 +5,9 @@
 
 Pokemon::Pokemon()
 {
-	
+
 }
-Pokemon::Pokemon(int ID, std:: string nom, std:: string type, int posx, int posy, int pvmax, int atk, int atkspe, int def, int defspe, int vit,  int evdonne, std:: string typeev)
+Pokemon::Pokemon(int ID, std::string nom, std::string type, int posx, int posy, int pvmax, int atk, int atkspe, int def, int defspe, int vit, int evdonne, std::string typeev)
 {
 	p_ID = ID;
 	std::cout << p_ID << std::endl;
@@ -23,13 +23,13 @@ Pokemon::Pokemon(int ID, std:: string nom, std:: string type, int posx, int posy
 	p_vit = vit;
 	//p_rare = 1;//on en aura pas besoin
 	p_evdonne = evdonne;
-	p_typeev = typeev; 
+	p_typeev = typeev;
 	/*sf::Sprite p_sprite;
 	sf::Texture p_texture;
 	p_texture.loadFromFile("Sprite/4g_sprite_pokemon.png", sf::IntRect(p_posx, p_posy, 80, 80));
 	p_sprite.setTexture(p_texture);*/
 	//on va le charger sur une autre fonciton pour les temps de calcul lors de chargement
-	if (type=="normal") {
+	if (type == "normal") {
 		p_nombretype = 0;
 	}
 	if (type == "feu") {
@@ -87,11 +87,11 @@ Pokemon::~Pokemon()
 
 }
 
-std:: string Pokemon::p_getnom()
+std::string Pokemon::p_getnom()
 {
 	return p_nom;
 }
-std:: string Pokemon::p_gettype()
+std::string Pokemon::p_gettype()
 {
 	return p_type;
 }
@@ -129,13 +129,13 @@ int Pokemon::p_getvit()
 }
 /*int Pokemon::p_getrare()
 {
-	return p_rare;
+return p_rare;
 }*/
 int Pokemon::p_getevdonne()
 {
 	return p_evdonne;
 }
-std:: string Pokemon::p_gettypeev()
+std::string Pokemon::p_gettypeev()
 {
 	return p_typeev;
 }
@@ -150,13 +150,28 @@ int Pokemon::p_getid()
 sf::Sprite Pokemon::p_getsprite() {
 	return p_sprite;
 }
-void Pokemon::p_setsprite(int x) {//x étant le facteur graphique
+void Pokemon::p_setsprite(int x) {//x Ã©tant le facteur graphique
 	p_texture.loadFromFile("Sprite/4g_sprite_pokemon.png", sf::IntRect(p_posx, p_posy, 80, 80));
 	p_sprite.setTexture(p_texture);
-	p_sprite.setScale(2*x, 2*x);
+	p_sprite.setScale(2 * x, 2 * x);
 }
 void Pokemon::p_setSpritePosition(float x, float y, float facteurG)
 {
 	p_sprite.setPosition(facteurG*x, facteurG*y);
 }
 
+std::string Pokemon::p_getAffichage()
+{
+	std::string textString;
+	textString = "ID: " + std::to_string(p_ID)
+		+ "\nType:\t" + p_type
+		+ "\nAttaque:\t" + std::to_string(p_atk) + "\tAttaque Speciale:\t" + std::to_string(p_atkspe)
+		+ "\nDefense:\t" + std::to_string(p_def) + "\tDefense Speciale:\t" + std::to_string(p_defspe)
+		+ "\nVitesse:\t" + std::to_string(p_vit) + "\tPV:\t" + std::to_string(p_pvmax)
+		+ "\nPress S to sort options";
+	/*sf::Text text((sf::String) textString, font, 20);
+
+	text.setPosition(610, 740);
+	text.setFillColor(sf::Color::White);*/
+	return textString;
+}
